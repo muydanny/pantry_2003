38f68798028a1b9a134cdb08d1ddf8303e55d19e
Danny Ramos <danramos21@gmail.com>
2020-04-22 09:39:11 -0600

    Recipe class and test

---
+++ test/recipe_test.rb
@@ -0,0 +1,37 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/recipe'
+
+class RecipeTest < Minitest::Test
+  def setup
+    @recipe1 = Recipe.new("Mac and Cheese")
+    @recipe2 = Recipe.new("Cheese Burger")
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    end
+
+    def test_it_exists
+      assert_instance_of Recipe, @recipe1
+    end
+
+    def test_it_has_attributes
+      assert_equal "Mac and Cheese", @recipe1.name
+      assert_equal ({}), @recipe1.ingredients_required
+    end
+
+    def test_it_can_add_ingredients_to_required
+      @recipe1.add_ingredient(@ingredient1, 2)
+      @recipe1.add_ingredient(@ingredient1, 4)
+      @recipe1.add_ingredient(@ingredient2, 8)
+      expected = {@ingredient1 => 6, @ingredient2 => 8}
+      assert_equal expected, @recipe1.ingredients_required
+    end
+
+    def test_it_can_check_ingredients
+      @recipe1.add_ingredient(@ingredient1, 2)
+      @recipe1.add_ingredient(@ingredient1, 4)
+      @recipe1.add_ingredient(@ingredient2, 8)
+      assert_equal [@ingredient1, @ingredient2], @recipe1.ingredients
+    end
+  end
