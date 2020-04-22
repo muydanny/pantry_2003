b3df02801ec4cc6d08d9ce312762823ac546ee8d
Danny Ramos <danramos21@gmail.com>
2020-04-22 09:08:12 -0600

    Ingredient class setup

---
+++ test/ingredient_test.rb
@@ -0,0 +1,21 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/pantry'
+
+class IngredientTest < Minitest::Test
+  def setup
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    @ingredient2 = Ingredient.new("Macaroni", "oz", 200)
+    end
+
+    def test_it_exists
+      assert_instance_of Ingredient, @ingredient1
+    end
+
+    def test_it_has_attributes
+      assert_equal "Cheese", @ingredient1.name
+      assert_equal "oz", @ingredient1.unit
+      assert_equal 50, @ingredient1.calories
+    end
+  end
