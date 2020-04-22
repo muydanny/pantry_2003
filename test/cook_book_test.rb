56f816632efac7a055ae496bbca35e74e95d0257
Danny Ramos <danramos21@gmail.com>
2020-04-22 09:43:54 -0600

    Cookbook class and tests

---
+++ test/cook_book_test.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/recipe'
+require './lib/cook_book'
+
+class CookBookTest < Minitest::Test
+  def setup
+    @cookbook = CookBook.new
+    @recipe1 = Recipe.new("Mac and Cheese")
+    @recipe2 = Recipe.new("Cheese Burger")
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    end
+
+    def test_it_exists
+      assert_instance_of CookBook, @cookbook
+    end
+
+    def test_it_has_attributes
+      assert_equal [], @cookbook.recipes
+    end
+
+    def test_it_can_add_recipes
+      @cookbook.add_recipe(@recipe1)
+      @cookbook.add_recipe(@recipe2)
+      assert_equal [@recipe1, @recipe2], @cookbook.recipes
+    end
+end
