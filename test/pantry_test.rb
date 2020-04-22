ce7f820186cf9b05a3f69119ab3d1e91b4255428
Danny Ramos <danramos21@gmail.com>
2020-04-22 09:25:37 -0600

    Updated previous commit

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -7,7 +7,7 @@ class PantryTest < Minitest::Test
   def setup
     @pantry = Pantry.new
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
-    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
     end
 
     def test_it_exists
